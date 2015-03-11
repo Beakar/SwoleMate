abf8e59768207f2b75fadbf64c746ba6236d40bb
jonesd14@up.edu <jonesd14@up.edu>
2015-03-11 11:55:55 -0700

    FoodTrackingActivity: searching and populating text fields for food presets is now working

--- app/src/main/java/edu/up/swolemate/FoodPresets.java
+++ app/src/main/java/edu/up/swolemate/FoodPresets.java
@@ -2,6 +2,8 @@ package edu.up.swolemate;
 
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 
 /**
  * Created by Daniel on 2/5/2015.
@@ -11,19 +13,13 @@ public class FoodPresets {
      * The ArrayList containing the preset food items.
      */
     protected ArrayList<FoodItem> foodPresets;
-
+    protected Hashtable<String, FoodItem> presetsTable;
 
     /**
      * Initializes an empty FoodPresets object
      */
     public FoodPresets(){
-    }
-
-
-    /**
-     * Creates the food presets and stores them into the foodPresets ArrayList.
-     */
-    protected void createFoodPresets(){
+        foodPresets = new ArrayList<FoodItem>();
         /**
          * Creates all of the FoodItem presets, and sets their values for the FoodItem(s) (by calling setFoodPresetValues()),
          *     and then stores the FoodItem in the foodPresets ArrayList.
@@ -31,223 +27,244 @@ public class FoodPresets {
          *     carbs, protein and serving size (in oz.).
          */
         FoodItem chickenBreast = new FoodItem();
-        setFoodPresetValues(chickenBreast, "Chicken Breast", 31, 0.76, 0.0, 5.76, 1.0);
+        chickenBreast = setFoodPresetValues(chickenBreast, "Chicken Breast", 31, 0.76, 0.0, 5.76, 1.0);
         foodPresets.add(chickenBreast);
 
         FoodItem steakTopSirloin = new FoodItem();
-        setFoodPresetValues(steakTopSirloin, "Steak (Top Sirloin, trimmed to 0\"\" fat)",
+        steakTopSirloin = setFoodPresetValues(steakTopSirloin, "Steak (Top Sirloin, trimmed to 0\"\" fat)",
                 60, 2.74, 0.0, 8.34, 1.0);
         foodPresets.add(steakTopSirloin);
 
         FoodItem steakRibeye = new FoodItem();
-        setFoodPresetValues(steakRibeye, "Steak (Ribeye, trimmed to 0\"\" fat)", 51,
+        steakRibeye = setFoodPresetValues(steakRibeye, "Steak (Ribeye, trimmed to 0\"\" fat)", 51,
                 3.0, 0.43, 8.34, 1.0);
         foodPresets.add(steakRibeye);
 
         FoodItem codAtlantic = new FoodItem();
-        setFoodPresetValues(codAtlantic, "Cod (Atlantic)", 23, 0.19, 0.0, 5.05, 1.0);
+        codAtlantic = setFoodPresetValues(codAtlantic, "Cod (Atlantic)", 23, 0.19, 0.0, 5.05, 1.0);
         foodPresets.add(codAtlantic);
 
         FoodItem codPacific = new FoodItem();
-        setFoodPresetValues(codPacific, "Cod (Pacific)", 24, 0.12, 0.0, 4.33, 1.0);
+        codPacific = setFoodPresetValues(codPacific, "Cod (Pacific)", 24, 0.12, 0.0, 4.33, 1.0);
         foodPresets.add(codPacific);
 
         FoodItem tilapia = new FoodItem();
-        setFoodPresetValues(tilapia, "Tilapia", 36, 1.0, 0.0, 7.0, 1.0);
+        tilapia = setFoodPresetValues(tilapia, "Tilapia", 36, 1.0, 0.0, 7.0, 1.0);
         foodPresets.add(tilapia);
 
         FoodItem salmonAtlantic = new FoodItem();
-        setFoodPresetValues(salmonAtlantic, "Salmon (Atlantic, wild)", 40, 1.8,
+        salmonAtlantic = setFoodPresetValues(salmonAtlantic, "Salmon (Atlantic, wild)", 40, 1.8,
                 0.0, 5.62, 1.0);
         foodPresets.add(salmonAtlantic);
 
         FoodItem salmonSockeye = new FoodItem();
-        setFoodPresetValues(salmonSockeye, "Salmon (Sockeye)", 40, 1.59, 0.0,
+        salmonSockeye = setFoodPresetValues(salmonSockeye, "Salmon (Sockeye)", 40, 1.59, 0.0,
                 6.0, 1.0);
         foodPresets.add(salmonSockeye);
 
         FoodItem salmonPink = new FoodItem();
-        setFoodPresetValues(salmonPink, "Salmon (Pink)", 36, 1.25, 0.0, 5.81, 1.0);
+        salmonPink = setFoodPresetValues(salmonPink, "Salmon (Pink)", 36, 1.25, 0.0, 5.81, 1.0);
         foodPresets.add(salmonPink);
 
         FoodItem salmonKing = new FoodItem();
-        setFoodPresetValues(salmonKing, "Salmon (King, Chinook, Alaska Native)",
+        salmonKing = setFoodPresetValues(salmonKing, "Salmon (King, Chinook, Alaska Native)",
                 52, 3, 0.0, 6.0, 1.0);
         foodPresets.add(salmonKing);
 
         FoodItem appleFuji = new FoodItem();
-        setFoodPresetValues(appleFuji, "Apple (Fuji, large (236g)", 149, 0.42,
+        appleFuji = setFoodPresetValues(appleFuji, "Apple (Fuji, large (236g)", 149, 0.42,
                 35.92, 0.47, 8.32);
         foodPresets.add(appleFuji);
 
         FoodItem appleGala = new FoodItem();
-        setFoodPresetValues(appleGala, "Apple (Gala, large (200g)", 114, 0.24,
+        appleGala = setFoodPresetValues(appleGala, "Apple (Gala, large (200g)", 114, 0.24,
                 27.36, 0.7, 7.05);
         foodPresets.add(appleGala);
 
         FoodItem appleGoldenDelicious = new FoodItem();
-        setFoodPresetValues(appleGoldenDelicious, "Apple (Golden Delicious, large (215g))",
+        appleGoldenDelicious = setFoodPresetValues(appleGoldenDelicious, "Apple (Golden Delicious, large (215g))",
                 123, 0.32, 29.24, 0.6, 7.58);
         foodPresets.add(appleGoldenDelicious);
 
         FoodItem appleGrannySmith = new FoodItem();
-        setFoodPresetValues(appleGrannySmith, "Apple (Granny Smith, large (206g)",
+        appleGrannySmith = setFoodPresetValues(appleGrannySmith, "Apple (Granny Smith, large (206g)",
                 119, 0.39, 28.04, 0.91, 7.27);
         foodPresets.add(appleGrannySmith);
 
         FoodItem strawberries = new FoodItem();
-        setFoodPresetValues(strawberries, "Strawberries, 1 cup (4 oz.)", 46, 0.43,
+        strawberries = setFoodPresetValues(strawberries, "Strawberries, 1 cup (4 oz.)", 46, 0.43,
                 11.06, 0.96, 4.02);
         foodPresets.add(strawberries);
 
         FoodItem banana = new FoodItem();
-        setFoodPresetValues(banana, "Banana, large (136g)", 121, 0.45, 31.06, 1.48,
+        banana = setFoodPresetValues(banana, "Banana, large (136g)", 121, 0.45, 31.06, 1.48,
                 4.8);
         foodPresets.add(banana);
 
         FoodItem orangeNavel = new FoodItem();
-        setFoodPresetValues(orangeNavel, "Orange (Navel, whole)", 81, 0.25, 20.69,
+        orangeNavel = setFoodPresetValues(orangeNavel, "Orange (Navel, whole)", 81, 0.25, 20.69,
                 1.5, 4.94);
         foodPresets.add(orangeNavel);
 
         FoodItem orangeFlorida = new FoodItem();
-        setFoodPresetValues(orangeFlorida, "Orange (Florida, whole (140g)", 85,
+        orangeFlorida = setFoodPresetValues(orangeFlorida, "Orange (Florida, whole (140g)", 85,
                 0.39, 21.35, 1.3, 4.97);
         foodPresets.add(orangeFlorida);
 
         FoodItem basil = new FoodItem();
-        setFoodPresetValues(basil, "Basil (fresh, 1 leaf)", 1, 0.02, 0.07, 0.08,
+        basil = setFoodPresetValues(basil, "Basil (fresh, 1 leaf)", 1, 0.02, 0.07, 0.08,
                 0.09);
         foodPresets.add(basil);
 
         FoodItem beansBlack = new FoodItem();
-        setFoodPresetValues(beansBlack, "Beans (black, 1 cup (172g)", 227, 0.93,
+        beansBlack = setFoodPresetValues(beansBlack, "Beans (black, 1 cup (172g)", 227, 0.93,
                 40.78, 8.86, 6.07);
         foodPresets.add(beansBlack);
 
         FoodItem beansKidney = new FoodItem();
-        setFoodPresetValues(beansKidney, "Beans (Kidney, 100g)", 33, 0.58, 4.72,
+        beansKidney = setFoodPresetValues(beansKidney, "Beans (Kidney, 100g)", 33, 0.58, 4.72,
                 4.82, 3.53);
         foodPresets.add(beansKidney);
 
         FoodItem beansPinto = new FoodItem();
-        setFoodPresetValues(beansPinto, "Beans (Pinto, 1 cup (171g)", 108, 1.11,
+        beansPinto = setFoodPresetValues(beansPinto, "Beans (Pinto, 1 cup (171g)", 108, 1.11,
                 44.84, 15.41, 6.03);
         foodPresets.add(beansPinto);
 
         FoodItem bellPepperGreen = new FoodItem();
-        setFoodPresetValues(bellPepperGreen, "Bell Pepper (red, large (164g)", 33,
+        bellPepperGreen = setFoodPresetValues(bellPepperGreen, "Bell Pepper (red, large (164g)", 33,
                 0.28, 7.61, 1.41, 5.78);
         foodPresets.add(bellPepperGreen);
 
         FoodItem bellPepperRed = new FoodItem();
-        setFoodPresetValues(bellPepperRed, "Bell Pepper (red, large (164g)", 51,
+        bellPepperRed = setFoodPresetValues(bellPepperRed, "Bell Pepper (red, large (164g)", 51,
                 0.49, 9.89, 1.62, 5.78);
         foodPresets.add(bellPepperRed);
 
         FoodItem bellPepperYellow = new FoodItem();
-        setFoodPresetValues(bellPepperYellow, "Bell Pepper (yellow, large (186g)",
+        bellPepperYellow = setFoodPresetValues(bellPepperYellow, "Bell Pepper (yellow, large (186g)",
                 50, 0.39, 11.76, 1.86, 6.56);
         foodPresets.add(bellPepperYellow);
 
         FoodItem broccoli = new FoodItem();
-        setFoodPresetValues(broccoli, "Broccoli, 1 cup (91g)", 31, 0.34, 6.04,
+        broccoli = setFoodPresetValues(broccoli, "Broccoli, 1 cup (91g)", 31, 0.34, 6.04,
                 2.57, 3.2);
         foodPresets.add(broccoli);
 
         FoodItem corn = new FoodItem();
-        setFoodPresetValues(corn, "Corn (sweet, yellow)", 125, 1.96, 27.12,
+        corn = setFoodPresetValues(corn, "Corn (sweet, yellow)", 125, 1.96, 27.12,
                 4.74, 5.11);
         foodPresets.add(corn);
 
         FoodItem garlic = new FoodItem();
-        setFoodPresetValues(garlic, "Garlic (raw, 1 clove)", 4, 0.02, 0.99,
+        garlic = setFoodPresetValues(garlic, "Garlic (raw, 1 clove)", 4, 0.02, 0.99,
                 0.19, 0.11);
         foodPresets.add(garlic);
 
         FoodItem lettuceIceberg = new FoodItem();
-        setFoodPresetValues(lettuceIceberg, "Lettuce (Iceberg, 1 cup chopped (57g)",
+        lettuceIceberg = setFoodPresetValues(lettuceIceberg, "Lettuce (Iceberg, 1 cup chopped (57g)",
                 8, 0.08, 1.69, 0.51, 2.01);
         foodPresets.add(lettuceIceberg);
 
         FoodItem lettuceRomaine = new FoodItem();
-        setFoodPresetValues(lettuceRomaine, "Lettuce (Romaine, 1 cup shredded(47g))",
+        lettuceRomaine = setFoodPresetValues(lettuceRomaine, "Lettuce (Romaine, 1 cup shredded(47g))",
                 8, 0.14, 1.55, 0.58, 1.66);
         foodPresets.add(lettuceRomaine);
 
         FoodItem onion = new FoodItem();
-        setFoodPresetValues(onion, "Onion, 1 whole (150g)", 60, 0.15, 14.01,
+        onion = setFoodPresetValues(onion, "Onion, 1 whole (150g)", 60, 0.15, 14.01,
                 1.65, 5.29);
         foodPresets.add(onion);
 
         FoodItem onionSweet = new FoodItem();
-        setFoodPresetValues(onionSweet, "Onion (sweet, 1 whole (331g)", 106,
+        onionSweet = setFoodPresetValues(onionSweet, "Onion (sweet, 1 whole (331g)", 106,
                 0.26, 24.99, 2.62, 5.29);
         foodPresets.add(onionSweet);
 
         FoodItem parsley = new FoodItem();
-        setFoodPresetValues(parsley, "Parsley (fresh (1 tbsp)", 1, 0.03, 0.24,
+        parsley = setFoodPresetValues(parsley, "Parsley (fresh (1 tbsp)", 1, 0.03, 0.24,
                 0.11, 0.13);
         foodPresets.add(parsley);
 
         FoodItem potatoRusset = new FoodItem();
-        setFoodPresetValues(potatoRusset, "Potato (Russet (100g)", 79, 0.08,
+        potatoRusset = setFoodPresetValues(potatoRusset, "Potato (Russet (100g)", 79, 0.08,
                 18.07, 2.14, 3.53);
         foodPresets.add(potatoRusset);
 
         FoodItem potatoRed = new FoodItem();
-        setFoodPresetValues(potatoRed, "Potato (red (100g)", 70, 0.14, 15.9,
+        potatoRed = setFoodPresetValues(potatoRed, "Potato (red (100g)", 70, 0.14, 15.9,
                 1.89, 3.53);
         foodPresets.add(potatoRed);
 
         FoodItem spinach = new FoodItem();
-        setFoodPresetValues(spinach, "Spinach (raw, 1 cup (30g)", 7, 0.12,
+        spinach = setFoodPresetValues(spinach, "Spinach (raw, 1 cup (30g)", 7, 0.12,
                 1.09, 0.86, 1.06);
         foodPresets.add(spinach);
 
         FoodItem sweetPotato = new FoodItem();
-        setFoodPresetValues(sweetPotato, "Sweet Potato (100g)", 86, 0.05,
+        sweetPotato = setFoodPresetValues(sweetPotato, "Sweet Potato (100g)", 86, 0.05,
                 20.12, 1.57, 3.53);
         foodPresets.add(sweetPotato);
 
         FoodItem tomato = new FoodItem();
-        setFoodPresetValues(tomato, "Tomato (1 large (182g))", 33, 0.36,
+        tomato = setFoodPresetValues(tomato, "Tomato (1 large (182g))", 33, 0.36,
                 7.08, 1.6, 6.42);
         foodPresets.add(tomato);
 
         FoodItem cayennePepper = new FoodItem();
-        setFoodPresetValues(cayennePepper, "Cayenne Pepper (1 tsp))", 6,
+        cayennePepper = setFoodPresetValues(cayennePepper, "Cayenne Pepper (1 tsp))", 6,
                 0.31, 1.02, 0.22, 0.06);
         foodPresets.add(cayennePepper);
 
         FoodItem egg = new FoodItem();
-        setFoodPresetValues(egg, "Egg (whole, large (50g))", 72, 4.76,
+        egg = setFoodPresetValues(egg, "Egg (whole, large (50g))", 72, 4.76,
                 0.36, 6.28, 1.76);
         foodPresets.add(egg);
 
         FoodItem eggWhite = new FoodItem();
-        setFoodPresetValues(eggWhite, "Egg White (large (33g))", 17,
+        eggWhite = setFoodPresetValues(eggWhite, "Egg White (large (33g))", 17,
                 0.06, 0.27, 3.6, 1.64);
         foodPresets.add(eggWhite);
 
         FoodItem garlicPowder = new FoodItem();
-        setFoodPresetValues(garlicPowder, "Garlic Powder", 10, 0.02,
+        garlicPowder = setFoodPresetValues(garlicPowder, "Garlic Powder", 10, 0.02,
                 2.25, 0.0, 0.11);
         foodPresets.add(garlicPowder);
 
         FoodItem blackPepper = new FoodItem();
-        setFoodPresetValues(blackPepper, "Black Pepper (ground)", 5,
+        blackPepper = setFoodPresetValues(blackPepper, "Black Pepper (ground)", 5,
                 0.07, 1.47, 0.24, 0.08);
         foodPresets.add(blackPepper);
 
         FoodItem whitePepper = new FoodItem();
-        setFoodPresetValues(whitePepper, "White Pepper (ground)", 7,
+        whitePepper = setFoodPresetValues(whitePepper, "White Pepper (ground)", 7,
                 0.05, 1.65, 0.25, 0.08);
         foodPresets.add(whitePepper);
 
         FoodItem salt = new FoodItem();
-        setFoodPresetValues(salt, "Salt (table)", 0, 0.0, 0.0, 0.0, 0.21);
+         salt = setFoodPresetValues(salt, "Salt (table)", 0, 0.0, 0.0, 0.0, 0.21);
         foodPresets.add(salt);
+    }
+
+
+    /**
+     * Creates the food presets and stores them into the foodPresets ArrayList.
+     */
+    protected void createFoodPresets(){
+
+    }
+
+
+    /**
+     * Returns the ArrayList of foodType names of foodPresets.
+     * @return -- The ArrayList of foodTypes (String)
+     */
+    protected ArrayList<String> getFoodTypes(){
+        ArrayList<String> list = new ArrayList<String>();
+        for(FoodItem item : foodPresets){
+            list.add(item.getFoodType());
+        }
+        return list;
     }
 
 
@@ -265,6 +282,26 @@ public class FoodPresets {
 
 
     /**
+     * Gets the FoodPresets table.
+     * @return -- The FoodPresets Hashtable.
+     */
+    protected Hashtable getPresetsTable(){
+        return this.presetsTable;
+    }
+
+
+    /**
+     * Initializes the FoodPresets table.
+     */
+    protected void initPresetsTable(){
+        presetsTable = new Hashtable<String, FoodItem>();
+        for(FoodItem food : foodPresets){
+            presetsTable.put(food.getFoodType(), food);
+        }
+    }
+
+
+    /**
      * Sets the values for the FoodItem.
      *
      * @param food -- The FoodItem
@@ -275,7 +312,7 @@ public class FoodPresets {
      * @param protein -- Protein amount
      * @param servingSize -- Serving size( in oz.)
      */
-    protected void setFoodPresetValues(FoodItem food, String name, int cal, double fat, double carbs, double protein, double servingSize){
+    protected FoodItem setFoodPresetValues(FoodItem food, String name, int cal, double fat, double carbs, double protein, double servingSize){
         //Set all of the valus
         food.setCalories(cal);
         food.setFoodType(name);
@@ -283,5 +320,6 @@ public class FoodPresets {
         food.setCarbs(carbs);
         food.setProtein(protein);
         food.setServingSize(servingSize);
+        return food;
     }
 }
--- app/src/main/java/edu/up/swolemate/FoodTrackingActivity.java
+++ app/src/main/java/edu/up/swolemate/FoodTrackingActivity.java
@@ -4,14 +4,70 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
+import java.util.Hashtable;
+import java.util.List;
 
-public class FoodTrackingActivity extends Activity {
+public class FoodTrackingActivity extends Activity implements OnClickListener, AdapterView.OnItemClickListener {
+    AutoCompleteTextView foodAutoEditText;
+    Spinner foodSpinner;
+    EditText srvSizeEditText;
+    EditText calEditText;
+    EditText fatEditText;
+    EditText carbsEditText;
+    EditText proteinEditText;
+    EditText servingsEditText;
+    ArrayAdapter arrayAdapter;
+    List<String> names;
+    Button saveButton;
+    Button delPresetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.food_tracking_activity);
+
+        /*
+         * This section creates an array adapter, which populates the AutoCompleteEditText
+         * with the value from the FoodPresets
+         */
+        foodAutoEditText = (AutoCompleteTextView)findViewById(R.id.foodAutoEditText);
+        FoodPresets presets = new FoodPresets();
+        names = presets.getFoodTypes();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, names);
+        foodAutoEditText.setAdapter(arrayAdapter);
+        foodAutoEditText.setOnItemClickListener(this);
+        //initialize all of the EditTexts
+        srvSizeEditText = (EditText)findViewById(R.id.servingSizeEditText);
+        calEditText = (EditText)findViewById(R.id.caloriesEditText);
+        fatEditText = (EditText)findViewById(R.id.fatEditText);
+        carbsEditText = (EditText)findViewById(R.id.carbsEditText);
+        proteinEditText = (EditText)findViewById(R.id.proteinEditText);
+        servingsEditText = (EditText)findViewById(R.id.servingsEditText);
+        //initialize the buttons and set the onClickListener
+        saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(this);
+        delPresetButton = (Button)findViewById(R.id.deletePresetButton);
+        delPresetButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.foodAutoEditText && !foodAutoEditText.getText().toString().equals("")){
+            FoodPresets presets = new FoodPresets();
+            presets.initPresetsTable();
+            Hashtable<String, FoodItem> table = presets.getPresetsTable();
+            FoodItem food = table.get(foodAutoEditText.getText().toString());
+            srvSizeEditText.setText("" + food.getServingSize());
+        }
     }
 
 
@@ -20,6 +76,19 @@ public class FoodTrackingActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg4){
+        FoodPresets presets = new FoodPresets();
+        FoodItem food = presets.foodPresets.get(position);
+        srvSizeEditText.setText("" + food.getServingSize() + "oz.");
+        calEditText.setText("" + food.getCalories());
+        fatEditText.setText("" + food.getFat());
+        carbsEditText.setText("" + food.getCarbs());
+        proteinEditText.setText("" + food.getProtein());
+
     }
 
     @Override
