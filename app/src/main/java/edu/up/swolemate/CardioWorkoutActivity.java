e014e57abe1c2de67d780c0509c4b02a7b9caa9e
nateyb93 <brownn15@up.edu>
2015-04-12 14:46:13 -0700

    CardioWorkoutActivity layout updated with saving/checkbox functionality. Chronometer still always starts at 0

--- app/src/main/java/edu/up/swolemate/CardioWorkoutActivity.java
+++ app/src/main/java/edu/up/swolemate/CardioWorkoutActivity.java
@@ -3,13 +3,17 @@ package edu.up.swolemate;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.Chronometer;
 
 import android.content.ContentValues;
@@ -20,13 +24,21 @@ import android.database.sqlite.SQLiteDatabase;
 public class CardioWorkoutActivity extends Activity implements View.OnClickListener {
 
     FragmentManager fragManager;
-    Button finishButton;
+    FontButton finishButton;
     Button startTimerButton;
     Button stopTimerButton;
     Context context = this;
     Chronometer chronometer;
+
+    FontEditText workoutNameBox;
     FontEditText distanceBox;
     FontEditText durationBox;
+
+    CheckBox cb_manualInput;
+    CheckBox cb_recordDistance;
+
+    private boolean timerStopped = true;
+    private long stoppedTime = 0;
 
     protected CardioWorkout currentWorkout;
 
@@ -34,6 +46,9 @@ public class CardioWorkoutActivity extends Activity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_cardio_workout);
+
+        currentWorkout = new CardioWorkout();
+
         //lock the device in portrait mode
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -41,20 +56,31 @@ public class CardioWorkoutActivity extends Activity implements View.OnClickListe
         fragManager = getFragmentManager();
 
         chronometer = (Chronometer)findViewById(R.id.chronometer);
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                long time = getTimerValue();
+                chronometer.setText(String.format("%02d", time / 60) + ":" + String.format("%02d", time % 60));
+            }
+        });
         startTimerButton = (Button)findViewById(R.id.startButton);
         stopTimerButton = (Button)findViewById(R.id.stopButton);
-        finishButton = (Button)findViewById(R.id.finishButton);
-        distanceBox = (FontEditText)findViewById(R.id.cardioDistanceEditText);
+        finishButton = (FontButton)findViewById(R.id.finishButton);
+
+        distanceBox = (FontEditText)findViewById(R.id.et_cardioDistanceEditText);
         durationBox = (FontEditText)findViewById(R.id.durationEntry);
+        workoutNameBox = (FontEditText)findViewById(R.id.cardioNameEntry);
+
+        cb_manualInput = (CheckBox)findViewById(R.id.cb_manualInput);
+        cb_recordDistance = (CheckBox)findViewById(R.id.cb_recordDistance);
+
 
         startTimerButton.setOnClickListener(this);
         stopTimerButton.setOnClickListener(this);
-        finishButton.setOnClickListener(this);
         distanceBox.setOnClickListener(this);
         durationBox.setOnClickListener(this);
 
-        currentWorkout = new CardioWorkout("Test exercise");
-        currentWorkout.initTestValues();
+        initFocusListeners();
     }
 
     /*
@@ -63,22 +89,21 @@ public class CardioWorkoutActivity extends Activity implements View.OnClickListe
     public void onClick(View v){
         switch (v.getId()){
             case R.id.startButton:
-                chronometer.setBase(SystemClock.elapsedRealtime());
-                chronometer.start();
+                if(timerStopped)
+                    startTimer();
+                else
+                    stopTimer();
                 break;
             case R.id.stopButton:
-                chronometer.stop();
+                resetTimer();
                 break;
             case R.id.durationEntry:
                 double duration = Double.parseDouble(durationBox.getText().toString());
                 currentWorkout.setDuration(duration);
                 break;
-            case R.id.cardioDistanceEditText:
+            case R.id.et_cardioDistanceEditText:
                 double distance = Double.parseDouble(distanceBox.getText().toString());
                 currentWorkout.setDistance(distance);
-                break;
-            case R.id.finishButton:
-                Log.e("Logged Button pressed","finish");
                 break;
         }
     }
@@ -111,28 +136,100 @@ public class CardioWorkoutActivity extends Activity implements View.OnClickListe
     }
 
     /**
-     * Deletes all records from the CardioWorkouts table.
-     * Test function
+     * Initializes the listeners on the EditText fields in this activity
      */
-    private void deleteAll() {
-        SQLiteDatabase db = new FitnessDatabaseHelper(this).getReadableDatabase();
+    private void initFocusListeners() {
 
-        String query = "DELETE FROM CardioWorkouts";
-        db.execSQL(query);
+        durationBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b)
+                    cb_manualInput.setChecked(true);
+            }
+        });
+
+        distanceBox.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(b)
+                    cb_recordDistance.setChecked(true);
+            }
+        });
     }
 
+
+    private void updateWorkout() {
+        Editable duration = durationBox.getText();
+        Editable distance = distanceBox.getText();
+        Editable workoutName = workoutNameBox.getText();
+
+        if(cb_manualInput.isChecked()) {
+            if(duration != null)
+                currentWorkout.setDuration(Double.parseDouble(duration.toString()));
+        } else {
+            currentWorkout.setDuration(getTimerValue());
+        }
+
+        if(cb_recordDistance.isChecked()) {
+            if(distance != null)
+                currentWorkout.setDistance(Double.parseDouble(distance.toString()));
+        }
+
+        if(workoutName != null) {
+            currentWorkout.setDisplayName(workoutName.toString());
+        }
+    }
+
+    private long getTimerValue() {
+        return (SystemClock.elapsedRealtime() - chronometer.getBase()) / 1000;
+    }
     /**
      * Inserts the current workout object into the CardioWorkouts database
      */
     protected void insertCurrentWorkout() {
-        SQLiteDatabase db = new FitnessDatabaseHelper(this).getWritableDatabase();
-        ContentValues vals = new ContentValues();
+        updateWorkout();
+        new FitnessDatabaseHelper(this).insertWorkout(currentWorkout);
+    }
 
-        vals.put("displayName", currentWorkout.getDisplayName());
-        vals.put("duration", currentWorkout.getDuration());
-        vals.put("distance", currentWorkout.getDistance());
-        vals.put("dateCompleted", System.currentTimeMillis() / 1000);
+    /**
+     * Handles operations associated with starting the timer
+     */
+    public void startTimer() {
+        timerStopped = false;
+        chronometer.setBase(SystemClock.elapsedRealtime() + stoppedTime);
+        chronometer.start();
 
-        db.insert("CardioWorkouts", null, vals);
+        //update button to reflect new functionality
+        startTimerButton.setBackgroundColor(Color.parseColor("#ECF244"));
+        startTimerButton.setTextColor(Color.parseColor("#000000"));
+        startTimerButton.setText("Stop");
+    }
+
+    /**
+     * Handles operations associated with stopping the timer
+     */
+    public void stopTimer() {
+        timerStopped = true;
+        stoppedTime = getTimerValue();
+        chronometer.stop();
+
+        //update button to reflect new functionality
+        startTimerButton.setBackgroundColor(Color.parseColor("#009900"));
+        startTimerButton.setTextColor(Color.parseColor("#ffffff"));
+        startTimerButton.setText("Start");
+    }
+
+    /**
+     * Handles operations associated with resetting the timer
+     */
+    public void resetTimer() {
+        chronometer.setText("00:00");
+        stoppedTime = 0;
+    }
+
+    public void onFinishClick(View v) {
+        insertCurrentWorkout();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
